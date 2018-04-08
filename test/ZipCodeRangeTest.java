f1d033d3a0cec9a814f83bdf95f1f5a0a154b736
vkonde <vkonde@SFOC02Q18DDG8WN.sea.corp.expecn.com>
2018-04-08 11:25:41 -0700

    Added test case

---
+++ test/ZipCodeRangeTest.java
@@ -0,0 +1,14 @@
+class ZipCodeRangeTest {
+
+	@Test
+	void condenseXipCodeRangeListTest()
+       {	
+               ArrayList<ZipCodeRangeList> input = {"(91100,91200)", "(91150,91250)", "(91116,91231)","(94566,94566)","(94538,94568)"};
+	        ZipCodeRange obj= new ZipCodeRange();
+                ArrayList<ZipCodeRange>expected = {"(91100,91250)", "(94538,94568)"};
+		assertArrayEquals(expected, obj.condenseZipCodeRangeList(input));
+	}
+	
+
+}
+
