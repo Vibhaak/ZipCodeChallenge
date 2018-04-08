06a93708621eb2ee9e34b4cac4d501c5b493a679
vkonde <vkonde@SFOC02Q18DDG8WN.sea.corp.expecn.com>
2018-04-08 11:26:50 -0700

    Changed test case name

--- test/ZipCodeRangeTest.java
+++ test/ZipCodeRangeTest.java
@@ -1,7 +1,7 @@
 class ZipCodeRangeTest {
 
 	@Test
-	void condenseXipCodeRangeListTest()
+	void condenseZipCodeRangeListTest()
        {	
                ArrayList<ZipCodeRangeList> input = {"(91100,91200)", "(91150,91250)", "(91116,91231)","(94566,94566)","(94538,94568)"};
 	        ZipCodeRange obj= new ZipCodeRange();
